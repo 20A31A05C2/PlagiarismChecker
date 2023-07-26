8778fa7d8ed0df32f4b1148e45ea8a1022bf185e
Pothuraju Vineel <102516151+20A31A05C2@users.noreply.github.com>
2023-07-27 00:04:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from difflib import SequenceMatcher
 
-with open("1.txt", errors='ignore') as file1, open("2.txt", errors='ignore') as file2:
+with open("text files/1.txt", errors='ignore') as file1, open("text files/2.txt", errors='ignore') as file2:
     file1data = file1.read()
     file2data = file2.read()
     similarity = SequenceMatcher(None, file1data, file2data).ratio()
